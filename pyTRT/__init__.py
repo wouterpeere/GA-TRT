2c584426cd9d8774322f89616a93967f9fdb37f5
wouterpeere <wouter_peere@hotmail.com>
2025-07-18 10:28:16 +0200

    Fix test, add FOLDER

--- pyTRT/__init__.py
+++ pyTRT/__init__.py
@@ -1,1 +1,5 @@
+import pathlib
+
 from .utils import *
+
+FOLDER: pathlib.Path = pathlib.Path(__file__).parent
--- test/test_load_data.py
+++ test/test_load_data.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import pytest
 
-from pyTRT import TRTData
+from pyTRT import TRTData, FOLDER
 
 
 def test_p_average():
-    data = TRTData('data/test_linz.csv', 't [s]', 'Tf [degC]', col_power='P [W]', decimal=',')
+    data = TRTData(FOLDER.parent.joinpath("test/data/test_linz.csv"), 't [s]', 'Tf [degC]', col_power='P [W]',
+                   decimal=',')
     assert np.isclose(data.average_power, 7191.3840791032635)
